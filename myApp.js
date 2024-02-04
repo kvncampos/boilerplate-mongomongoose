ff201613253f2988f3b713e6d4f4fcf9c91e7a17
kvncampos <kvnelpdfw@hotmail.com>
2024-02-04 12:18:41 -0600

    Install and Set Up Mongoose

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-console.log("Hello World")
+let mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
