3ba925de911b0ca7d88dc8e81b8478a869b93ea1
kvncampos <kvnelpdfw@hotmail.com>
2024-02-04 12:26:32 -0600

    Connection Validation MondoDB

--- myApp.js
+++ myApp.js
@@ -1,6 +1,18 @@
 require('dotenv').config();
 let mongoose = require("mongoose");
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('Connected to Mongo!');
+    })
+    .catch((err) => {
+        console.error('Error connecting to Mongo', err);
+    });
+
 
 let Person;
 
